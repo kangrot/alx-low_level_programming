fe0f8f3cd61173a52cf9903b1c06bddc42f6cbd9
kangrot <kangrotnengak@gmail.com>
2023-08-11 10:08:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - using the printf function to print a string with a new line
- * Return : only 0
+ *
+ * Return: 0if successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
