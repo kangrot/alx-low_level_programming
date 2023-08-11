7144d064469ad775202e70fc66a03a30f10761e1
kangrot <kangrotnengak@gmail.com>
2023-08-11 09:51:09 -0700

    printf with new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using the printf function to print a string with a new line
+ * Return : only 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
