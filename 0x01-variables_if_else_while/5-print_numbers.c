22ab34953f214229b3a73c9b811cf4259ae1c829
kangrot <kangrotnengak@gmail.com>
2023-08-13 15:27:22 -0700

    printing base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing base 10 numbers from 1-9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
