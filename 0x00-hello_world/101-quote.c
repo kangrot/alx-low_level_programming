cd51471210c844183d5ab682f4296e5120ecfa97
kangrot <kangrotnengak@gmail.com>
2023-08-14 18:21:14 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - main function(entry point)
  *
- * return: always 1 if successful
+ * Return: always 1 if successful
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
