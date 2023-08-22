fe731f8998fd84e9f467200df30695249d3b657f
kangrot <kangrotnengak@gmail.com>
2023-08-22 00:34:40 -0700

    naturals

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,15 +8,25 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i;
+	unsigned long int j, k, next, sum;
 
-	for (i = 0; i < 1024; i++)
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
 
-	printf("%d\n", sum);
+	printf("%lu\n", sum);
 
 	return (0);
 }
