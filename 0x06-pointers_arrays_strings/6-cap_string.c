54f7408ea17ea0983900f2e5436d6c4d11ea00be
kangrot <kangrotnengak@gmail.com>
2023-08-28 11:03:03 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes words
+ * @s: string to be capitalized
+ *
+ * Return: returns the character
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 31;
+				}
+			}
+		}
+	}
+	return (s);
+}
