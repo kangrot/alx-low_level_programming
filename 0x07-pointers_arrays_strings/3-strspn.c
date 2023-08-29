c5898eb83580b872023321f35eff15d4a1d419d7
kangrot <kangrotnengak@gmail.com>
2023-08-28 18:49:32 -0700

    prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - gets the lenght of a prefix substring
+ * @s: string to check from
+ * @accept: string to search
+ *
+ * Return: number of matched characters
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] != accept[j]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
