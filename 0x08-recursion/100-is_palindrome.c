06ceaf02bd02eff18f73c27a84d70c876d0e40ac
kangrot <kangrotnengak@gmail.com>
2023-09-02 13:42:31 -0700

    lol

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,44 @@
 #include "main.h"
-
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- * can be considered identical, otherwise return 0.
- * @s1: string to compare to
- * @s2: string with wild character
+ * _strlen_recursion - gets string lenght
+ * @s: string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: lenght of string
  */
-int wildcmp(char *s1, char *s2)
+int _strlen_recursion(char *s)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 == '*')
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * compare_string - compare each character of the string
+`* @s: string
+ * @left: smallest iterator
+ * @right: largest iterator
+ *
+ * Return: int
+ */
+int compare_string(char *s, int left, int right)
+{
+	if (*(s + left) == *(s + right))
 	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		if (left == right || left == right + 1)
 			return (1);
+		return (0 + compare_string(s, left + 1, right - 1));
 	}
-
 	return (0);
+}
+/**
+ * is_palindrome - checks if string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if string == palindrome
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (compare_string(s, 0, _strlen_recursion(s - 1)));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
 
 #endif
