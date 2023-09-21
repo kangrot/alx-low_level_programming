4a2d171f91635c42b8162adbf53c7103d51b5d76
kangrot <kangrotnengak@gmail.com>
2023-09-21 11:52:19 -0700

    number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,10 +12,11 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
+	{
 		count++;
-	if (h != NULL)
-		count++;
+		h = h->next;
+	}
 
 	return (count);
 }
