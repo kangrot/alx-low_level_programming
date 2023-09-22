ce4a492fa02faa2c9cdf42b0a8378b8eaddb5656
kangrot <kangrotnengak@gmail.com>
2023-09-22 13:42:58 -0700

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 
-	new_node =(list_t *) malloc(sizeof(list_t));
+	new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
